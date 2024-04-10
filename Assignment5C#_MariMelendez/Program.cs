dd3901ccd5a9f438e5af99d9b8e9d1b2654171ca
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-04-10 19:49:59 -0400

    Mail Class Code

--- Assignment5C#_MariMelendez/Program.cs
+++ Assignment5C#_MariMelendez/Program.cs
@@ -11,5 +11,26 @@ namespace Assignment5C__MariMelendez
         static void Main(string[] args)
         {
         }
+
+        abstract class Mail
+        {
+            // Properties to set and get fields
+            public double Weight { get; set; }
+            private bool Express { get; set; }
+            private string DestinationAddress { get; set; }
+
+            // Mail constructor
+            protected Mail(double weight, bool express, string destinationAddress)
+            {
+                Weight = weight;
+                Express = express;
+                DestinationAddress = destinationAddress;
+            }
+
+            // CalculatePostage method, that will define on each one of the class childs. 
+            public abstract double CalculatePostage();
+
+
+        }
     }
 }
